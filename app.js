eca4d34f02686bc6f07cdd7c5d899932a9089ff0
Jamie vullo <jvullo@protonmail.com>
2020-07-19 12:36:59 -0400

    Just have to add some css.

--- app.js
+++ app.js
@@ -6,15 +6,14 @@ function submitExpense() {
     let addExpense = document.getElementById("submit-expense")
     addExpense.addEventListener('click', function(event) {
         event.preventDefault();
+        
         let expenseDate = document.getElementById("expense-date").value
-        console.log(expenseDate)
         let expenseDescription = document.getElementById("expense-description").value
-        console.log(expenseDescription)
         let expenseLocation = document.getElementById("expense-location").value
-        console.log(expenseLocation)
         let expenseAmount = document.getElementById("expense-amount").value
-        console.log(expenseAmount)
+        
         let values = [expenseDate, expenseDescription, expenseLocation, expenseAmount]
+        
         addExpenseToTable(values)
     })   
 }
@@ -22,7 +21,7 @@ function submitExpense() {
 function addExpenseToTable(values) {
     let expenseTable = document.getElementById("all-expenses")
     let expenseRow = document.createElement("tr")
-    // create delete button for every expense
+    
     let deleteButton = document.createElement("button")
     deleteButton.textContent = "Delete Expense"
     
@@ -35,8 +34,10 @@ function addExpenseToTable(values) {
         expenseRow.appendChild(deleteButton)
         expenseTable.appendChild(expenseRow)
     }
+
     let form = document.getElementById("form")
     form.reset()
+    
     deleteExpense(deleteButton, expenseRow)
 }
 
@@ -44,12 +45,4 @@ function deleteExpense(deleteButton, expenseRow) {
     deleteButton.addEventListener("click", function() {
         expenseRow.remove()
     })
-}
-
-function clearForm() {
-    let 
-}
-
-function sumExpenses() {
-    // would like to add total of all expenses
 }
