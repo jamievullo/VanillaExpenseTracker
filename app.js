e20967af45f7476795e3fd3dbd686505d397210d
Jamie vullo <jvullo@protonmail.com>
2020-07-19 12:33:07 -0400

    Added id's to inputs and moved value capture inside of listener

--- app.js
+++ app.js
@@ -3,26 +3,25 @@ document.addEventListener('DOMContentLoaded', function () {
 });
 
 function submitExpense() {
-    let addExpense = document.getElementById("form-box")
-    addExpense.addEventListener('submit', function(event) {
+    let addExpense = document.getElementById("submit-expense")
+    addExpense.addEventListener('click', function(event) {
         event.preventDefault();
-    })
-    // grab all inputted values
-    let expenseDate = document.getElementById("expense-date").value
-    console.log(expenseDate)
-    let expenseDescription = document.getElementById("expense-description").value
-    console.log(expenseDescription)
-    let expenseLocation = document.getElementById("expense-location").value
-    console.log(expenseLocation)
-    let expenseAmount = document.getElementById("expense-amount").value
-    console.log(expenseAmount)
-    let values = [expenseDate, expenseDescription, expenseLocation, expenseAmount]
-    addExpenseToTable(values)
+        let expenseDate = document.getElementById("expense-date").value
+        console.log(expenseDate)
+        let expenseDescription = document.getElementById("expense-description").value
+        console.log(expenseDescription)
+        let expenseLocation = document.getElementById("expense-location").value
+        console.log(expenseLocation)
+        let expenseAmount = document.getElementById("expense-amount").value
+        console.log(expenseAmount)
+        let values = [expenseDate, expenseDescription, expenseLocation, expenseAmount]
+        addExpenseToTable(values)
+    })   
 }
 
 function addExpenseToTable(values) {
     let expenseTable = document.getElementById("all-expenses")
-    let expenseRow = document.getElementById("tr")
+    let expenseRow = document.createElement("tr")
     // create delete button for every expense
     let deleteButton = document.createElement("button")
     deleteButton.textContent = "Delete Expense"
@@ -35,9 +34,9 @@ function addExpenseToTable(values) {
         expenseRow.appendChild(expenseList)
         expenseRow.appendChild(deleteButton)
         expenseTable.appendChild(expenseRow)
-
     }
-
+    let form = document.getElementById("form")
+    form.reset()
     deleteExpense(deleteButton, expenseRow)
 }
 
@@ -45,6 +44,10 @@ function deleteExpense(deleteButton, expenseRow) {
     deleteButton.addEventListener("click", function() {
         expenseRow.remove()
     })
+}
+
+function clearForm() {
+    let 
 }
 
 function sumExpenses() {
